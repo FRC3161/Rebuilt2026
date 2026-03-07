4ad9d451d719f2111f9ed74d27a541f800ec1a3b
PossibilityJoshuaB <pooria@pooria.dev>
2026-03-07 18:09:55 -0500

    added SOTF to hood and shooter

--- src/main/java/frc/robot/subsystems/Scoring/Shooter.java
+++ src/main/java/frc/robot/subsystems/Scoring/Shooter.java
@@ -215,38 +215,25 @@ public class Shooter extends SubsystemBase {
         position = 5.5;
         break;
       case HUB_SHOOTING:
-        motorspeed = ShooterConstants.shooterSpeedInterpolation
-          .getPrediction(
-            drivetrain.getCurrentTurretPose().getTranslation().getDistance(drivetrain.getHub()));
-        position = ShooterConstants.hoodAngleInterpolation
-          .getPrediction(
-            drivetrain.getCurrentTurretPose().getTranslation().getDistance(drivetrain.getHub()));
-        break;
+    Translation2d correctedVector = drivetrain.getSOTFTurretAngle("hub");
+    double correctedDistance = correctedVector.getNorm();
+
+    motorspeed = ShooterConstants.shooterSpeedInterpolation
+        .getPrediction(correctedDistance);
+
+    position = ShooterConstants.hoodAngleInterpolation
+        .getPrediction(correctedDistance);
+    break;
       case PASS_SHOOTING:
-        //determine passing spot
-        Translation2d passSpot;
-        if(DriverStation.getAlliance().get() == Alliance.Red) {
-                if(drivetrain.getPose().getY() > 4.03) {
-                    passSpot = VisionConstants.RED_PASS_SPOT_1;
-                } else {
-                    passSpot = VisionConstants.RED_PASS_SPOT_2;
-                }
-            } else {
-                if(drivetrain.getPose().getY() > 4.03) {
-                    passSpot = VisionConstants.BLUE_PASS_SPOT_1;
-                } else {
-                    passSpot = VisionConstants.BLUE_PASS_SPOT_2;
-                }
-            }
-        // use distance to passing spot for interpolation
-        motorspeed = ShooterConstants.shooterSpeedInterpolation
-          .getPrediction(
-            drivetrain.getCurrentTurretPose().getTranslation().getDistance(passSpot));
-        
-        position = ShooterConstants.hoodAngleInterpolation
-          .getPrediction(
-            drivetrain.getCurrentTurretPose().getTranslation().getDistance(passSpot));
-        break;
+         Translation2d correctedVector2 = drivetrain.getSOTFTurretAngle("pass");
+    double correctedDistance2 = correctedVector2.getNorm();
+
+    motorspeed = ShooterConstants.shooterSpeedInterpolation
+        .getPrediction(correctedDistance2);
+
+    position = ShooterConstants.hoodAngleInterpolation
+        .getPrediction(correctedDistance2);
+    break;
       case HOMING:
         position = -.1;
         if (hoodMotor.getSupplyCurrent().getValueAsDouble() >= ShooterConstants.homingThreshold) {
