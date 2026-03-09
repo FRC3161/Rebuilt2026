2c5f772f501c3ae77616fe48a144eba99ffd1718
PossibilityJoshuaB <pooria@pooria.dev>
2026-03-09 15:22:54 -0400

    Revert "Please don't break vision"
    This reverts commit 93daef3a219889e7eb068d8ebb93c50493f6fc52.

--- src/main/java/frc/robot/subsystems/Drive/Vision.java
+++ src/main/java/frc/robot/subsystems/Drive/Vision.java
@@ -1,5 +1,8 @@
 package frc.robot.subsystems.Drive;
 
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import org.photonvision.EstimatedRobotPose;
@@ -12,154 +15,304 @@ import edu.wpi.first.apriltag.AprilTagFieldLayout;
 import edu.wpi.first.apriltag.AprilTagFields;
 import edu.wpi.first.math.Matrix;
 import edu.wpi.first.math.VecBuilder;
-import edu.wpi.first.math.estimator.SwerveDrivePoseEstimator;
 import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Pose3d;
+import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.numbers.N1;
 import edu.wpi.first.math.numbers.N3;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.DriverStation.Alliance;
+import edu.wpi.first.wpilibj.smartdashboard.Field2d;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 import frc.robot.Constants.VisionConstants;
 
 public class Vision {
-
-    private final PhotonCamera camera1;
-    private final PhotonCamera camera2;
-    private final PhotonCamera camera3;
-
+    private final PhotonCamera camera1; //camera facing x
+    private final PhotonCamera camera2; //camera facing y
+    private final PhotonCamera camera3; //camera facing -y
     private final PhotonPoseEstimator poseEstimator1;
     private final PhotonPoseEstimator poseEstimator2;
     private final PhotonPoseEstimator poseEstimator3;
+    private double lastEstTimestamp1 = 0;
+    private double lastEstTimestamp2 = 0;
+    private double lastEstTimestamp3 = 0;
+    public Optional<EstimatedRobotPose> latestVision = Optional.empty();
+    public Optional<EstimatedRobotPose> latestVision2 = Optional.empty();
+    public Optional<EstimatedRobotPose> latestVision3 = Optional.empty();
+    public AprilTagFieldLayout kTagLayout;
 
-    private final AprilTagFieldLayout tagLayout;
+    
 
-    public Vision() {
-
-        tagLayout = AprilTagFieldLayout.loadField(AprilTagFields.k2026RebuiltWelded);
-
+    public Vision(){
+        kTagLayout = AprilTagFieldLayout.loadField(AprilTagFields.k2026RebuiltWelded);
         camera1 = new PhotonCamera(VisionConstants.cameraName);
         camera2 = new PhotonCamera(VisionConstants.camera2Name);
         camera3 = new PhotonCamera(VisionConstants.camera3Name);
 
-        poseEstimator1 =
-            new PhotonPoseEstimator(
-                tagLayout,
-                PoseStrategy.MULTI_TAG_PNP_ON_COPROCESSOR,
-                VisionConstants.kRobotToCam);
-
-        poseEstimator2 =
-            new PhotonPoseEstimator(
-                tagLayout,
-                PoseStrategy.MULTI_TAG_PNP_ON_COPROCESSOR,
-                VisionConstants.kRobotToCam2);
-
-        poseEstimator3 =
-            new PhotonPoseEstimator(
-                tagLayout,
-                PoseStrategy.MULTI_TAG_PNP_ON_COPROCESSOR,
-                VisionConstants.kRobotToCam3);
-
-        poseEstimator1.setMultiTagFallbackStrategy(PoseStrategy.LOWEST_AMBIGUITY);
+        poseEstimator1 = new PhotonPoseEstimator(kTagLayout, PoseStrategy.MULTI_TAG_PNP_ON_COPROCESSOR, VisionConstants.kRobotToCam);
+        poseEstimator2 = new PhotonPoseEstimator(kTagLayout, PoseStrategy.MULTI_TAG_PNP_ON_COPROCESSOR, VisionConstants.kRobotToCam2);
+        poseEstimator3 = new PhotonPoseEstimator(kTagLayout, PoseStrategy.MULTI_TAG_PNP_ON_COPROCESSOR, VisionConstants.kRobotToCam3);
+        poseEstimator1.setMultiTagFallbackStrategy(PoseStrategy.LOWEST_AMBIGUITY); //TODO: TRY TRIG SOLVE
         poseEstimator2.setMultiTagFallbackStrategy(PoseStrategy.LOWEST_AMBIGUITY);
         poseEstimator3.setMultiTagFallbackStrategy(PoseStrategy.LOWEST_AMBIGUITY);
     }
 
-   /**
- * Called from the Drive subsystem periodic
- */
-public void updateVision(CommandSwerveDrivetrain driveEstimator) {
+    public PhotonPipelineResult getLatestResult1() {
+        return camera1.getLatestResult();
+    }
 
-    processCamera(camera1, poseEstimator1, driveEstimator);
-    processCamera(camera2, poseEstimator2, driveEstimator);
-    processCamera(camera3, poseEstimator3, driveEstimator);
-}
+    public PhotonPipelineResult getLatestResult2() {
+        return camera2.getLatestResult();
+    }
 
-/**
- * Processes a single camera measurement
- */
-private void processCamera(
-    PhotonCamera camera,
-    PhotonPoseEstimator estimator,
-    CommandSwerveDrivetrain driveEstimator) {  // <--- changed type here
+    public PhotonPipelineResult getLatestResult3() {
+        return camera3.getLatestResult();
+    }
 
-    PhotonPipelineResult result = camera.getLatestResult();
+    public Optional<EstimatedRobotPose> getEstimatedGlobalPose1() {
+        // camera 1
+        var latestPoseEstimate1 = poseEstimator1.update(getLatestResult1());
+        double latestTimestamp = getLatestResult1().getTimestampSeconds();
+        boolean newResult = Math.abs(latestTimestamp - lastEstTimestamp1) > 1e-5;
+        if (newResult) {
+            lastEstTimestamp1 = latestTimestamp;
+        }
+        latestVision = latestPoseEstimate1;
+        return latestVision;
+    }
 
-    if (!result.hasTargets())
-        return;
+    public Optional<EstimatedRobotPose> getEstimatedGlobalPose2() {
+        // camera 2
+        var latestPoseEstimate2 = poseEstimator2.update(getLatestResult2());
+        double latestTimestamp = getLatestResult2().getTimestampSeconds();
+        boolean newResult = Math.abs(latestTimestamp - lastEstTimestamp2) > 1e-5;
+        if (newResult) {
+            lastEstTimestamp2 = latestTimestamp;
+        }
+        latestVision = latestPoseEstimate2;
+        return latestVision;
+    }
 
-    Optional<EstimatedRobotPose> estimate = estimator.update(result);
+    public Optional<EstimatedRobotPose> getEstimatedGlobalPose3() {
+        // camera 3
+        var latestPoseEstimate3 = poseEstimator3.update(getLatestResult3());
+        double latestTimestamp = getLatestResult3().getTimestampSeconds();
+        boolean newResult = Math.abs(latestTimestamp - lastEstTimestamp3) > 1e-5;
+        if (newResult) {
+            lastEstTimestamp3 = latestTimestamp;
+        }
+        latestVision = latestPoseEstimate3;
+        return latestVision;
+    }
 
-    if (estimate.isEmpty())
-        return;
+    public Pose2d getPoseFromCams() {
+        var visionEst = getEstimatedGlobalPose1();
+        var visionEst2 = getEstimatedGlobalPose2();
+        var visionEst3 = getEstimatedGlobalPose3();
+        Pose2d combinedEstimate = new Pose2d();
 
-    Pose2d visionPose = estimate.get().estimatedPose.toPose2d();
-    double timestamp = result.getTimestampSeconds();
+        if (visionEst.isPresent() && visionEst2.isPresent() && visionEst3.isPresent()) {
+            Pose2d poseFrom1 = visionEst.get().estimatedPose.toPose2d();
+            Pose2d poseFrom2 = visionEst2.get().estimatedPose.toPose2d();
+            Pose2d poseFrom3 = visionEst3.get().estimatedPose.toPose2d();
 
-    // Reject high ambiguity
-    if (result.getBestTarget().getPoseAmbiguity() > 0.2)
-        return;
+            double estXFrom1 = poseFrom1.getX();
+            double estXFrom2 = poseFrom2.getX();
+            double estXFrom3 = poseFrom3.getX();
 
-    // Reject impossible jumps
-    Pose2d currentPose = driveEstimator.getPose();  // <--- use getPose() from CommandSwerveDrivetrain
+            double estYFrom1 = poseFrom1.getY();
+            double estYFrom2 = poseFrom2.getY();
+            double estYFrom3 = poseFrom3.getY();
 
-    if (visionPose
-            .getTranslation()
-            .getDistance(currentPose.getTranslation())
-        > 2.0)
-        return;
+            double estCOSFrom1 = poseFrom1.getRotation().getCos();
+            double estCOSFrom2 = poseFrom2.getRotation().getCos();
+            double estCOSFrom3 = poseFrom3.getRotation().getCos();
 
-    Matrix<N3, N1> stdDevs = getEstimationStdDevs(result, visionPose);
+            double estSINFrom1 = poseFrom1.getRotation().getSin();
+            double estSINFrom2 = poseFrom2.getRotation().getSin();
+            double estSINFrom3 = poseFrom3.getRotation().getSin();
 
-    driveEstimator.addVisionMeasurement(
-        visionPose,
-        timestamp,
-        stdDevs);
-}
-    /**
-     * Calculates measurement noise based on tag count and distance
-     */
-    public Matrix<N3, N1> getEstimationStdDevs(
-        PhotonPipelineResult result,
-        Pose2d estimatedPose) {
+            double avgX = (estXFrom1 + estXFrom2 + estXFrom3) / 3;
+            double avgY = (estYFrom1 + estYFrom2 + estYFrom3) / 3;
 
+            double avgCos = (estCOSFrom1 + estCOSFrom2 + estCOSFrom3) / 3;
+            double avgSin = (estSINFrom1 + estSINFrom2 + estSINFrom3) / 3;
+
+            combinedEstimate = new Pose2d(avgX, avgY, new Rotation2d(avgCos, avgSin));
+        } else if (visionEst.isPresent() && (visionEst2.isEmpty())) {
+
+        } else if (visionEst2.isPresent() && (visionEst.isEmpty())) {
+
+        } else {
+            combinedEstimate = new Pose2d();
+        }
+        return combinedEstimate;
+    }
+
+    public Pose2d getPoseFromCams12() {
+        var visionEst = getEstimatedGlobalPose1();
+        var visionEst2 = getEstimatedGlobalPose2();
+        Pose2d combinedEstimate = new Pose2d();
+
+        if (visionEst.isPresent() && visionEst2.isPresent()) {
+            Pose2d poseFrom1 = visionEst.get().estimatedPose.toPose2d();
+            Pose2d poseFrom2 = visionEst2.get().estimatedPose.toPose2d();
+
+            double estXFrom1 = poseFrom1.getX();
+            double estXFrom2 = poseFrom2.getX();
+
+            double estYFrom1 = poseFrom1.getY();
+            double estYFrom2 = poseFrom2.getY();
+
+            double estCOSFrom1 = poseFrom1.getRotation().getCos();
+            double estCOSFrom2 = poseFrom2.getRotation().getCos();
+
+            double estSINFrom1 = poseFrom1.getRotation().getSin();
+            double estSINFrom2 = poseFrom2.getRotation().getSin();
+
+            double avgX = (estXFrom1 + estXFrom2 ) / 2;
+            double avgY = (estYFrom1 + estYFrom2 ) / 2;
+
+            double avgCos = (estCOSFrom1 + estCOSFrom2) / 2;
+            double avgSin = (estSINFrom1 + estSINFrom2) / 2;
+
+            combinedEstimate = new Pose2d(avgX, avgY, new Rotation2d(avgCos, avgSin));
+
+        } else {
+            combinedEstimate = new Pose2d();
+        }
+        return combinedEstimate;
+    }
+
+    public Pose2d getPoseFromCams23() {
+        var visionEst2 = getEstimatedGlobalPose2();
+        var visionEst3 = getEstimatedGlobalPose3();
+        Pose2d combinedEstimate = new Pose2d();
+
+        if ( visionEst2.isPresent() && visionEst3.isPresent()) {
+            Pose2d poseFrom2 = visionEst2.get().estimatedPose.toPose2d();
+            Pose2d poseFrom3 = visionEst3.get().estimatedPose.toPose2d();
+
+            double estXFrom2 = poseFrom2.getX();
+            double estXFrom3 = poseFrom3.getX();
+
+            double estYFrom2 = poseFrom2.getY();
+            double estYFrom3 = poseFrom3.getY();
+
+            double estCOSFrom2 = poseFrom2.getRotation().getCos();
+            double estCOSFrom3 = poseFrom3.getRotation().getCos();
+
+            double estSINFrom2 = poseFrom2.getRotation().getSin();
+            double estSINFrom3 = poseFrom3.getRotation().getSin();
+
+            double avgX = ( estXFrom2 + estXFrom3) / 2;
+            double avgY = ( estYFrom2 + estYFrom3) / 2;
+
+            double avgCos = ( estCOSFrom2 + estCOSFrom3) / 2;
+            double avgSin = ( estSINFrom2 + estSINFrom3) / 2;
+
+            combinedEstimate = new Pose2d(avgX, avgY, new Rotation2d(avgCos, avgSin));
+        } else {
+            combinedEstimate = new Pose2d();
+        }
+        return combinedEstimate;
+    }
+
+    public Pose2d getPoseFromCams13() {
+        var visionEst = getEstimatedGlobalPose1();
+        var visionEst3 = getEstimatedGlobalPose3();
+        Pose2d combinedEstimate = new Pose2d();
+
+        if (visionEst.isPresent() && visionEst3.isPresent()) {
+            Pose2d poseFrom1 = visionEst.get().estimatedPose.toPose2d();
+            Pose2d poseFrom3 = visionEst3.get().estimatedPose.toPose2d();
+
+            double estXFrom1 = poseFrom1.getX();
+            double estXFrom3 = poseFrom3.getX();
+
+            double estYFrom1 = poseFrom1.getY();
+            double estYFrom3 = poseFrom3.getY();
+
+            double estCOSFrom1 = poseFrom1.getRotation().getCos();
+            double estCOSFrom3 = poseFrom3.getRotation().getCos();
+
+            double estSINFrom1 = poseFrom1.getRotation().getSin();
+            double estSINFrom3 = poseFrom3.getRotation().getSin();
+
+            double avgX = (estXFrom1  + estXFrom3) / 2;
+            double avgY = (estYFrom1  + estYFrom3) / 2;
+
+            double avgCos = (estCOSFrom1 + estCOSFrom3) / 2;
+            double avgSin = (estSINFrom1 + estSINFrom3) / 2;
+
+            combinedEstimate = new Pose2d(avgX, avgY, new Rotation2d(avgCos, avgSin));
+        } else {
+            combinedEstimate = new Pose2d();
+        }
+        return combinedEstimate;
+    }
+
+    public Matrix<N3, N1> getEstimationStdDevs(Pose2d estimatedPose) {
         var estStdDevs = Constants.VisionConstants.kSingleTagStdDevs;
+        var targets1 = getLatestResult1().getTargets();
+        var targets2 = getLatestResult2().getTargets();
+        var targets3 = getLatestResult3().getTargets();
+        int numTags1 = 0;
+        int numTags2 = 0;
+        int numTags3 = 0;
+        double avgDist1 = 0;
+        double avgDist2 = 0;
+        double avgDist3 = 0;
 
-        int numTags = 0;
-        double avgDist = 0;
-
-        for (var tgt : result.getTargets()) {
-
-            var tagPose = tagLayout.getTagPose(tgt.getFiducialId());
-
-            if (tagPose.isEmpty())
+        for (var tgt1 : targets1) {
+            var tagPose = poseEstimator1.getFieldTags().getTagPose(tgt1.getFiducialId());
+            if (tagPose.isEmpty()){
                 continue;
-
-            numTags++;
-
-            avgDist += tagPose
-                .get()
-                .toPose2d()
-                .getTranslation()
-                .getDistance(estimatedPose.getTranslation());
+            }
+            numTags1++;
+            avgDist1 += tagPose.get().toPose2d().getTranslation().getDistance(estimatedPose.getTranslation());
+        }
+        for (var tgt2 : targets2) {
+            var tagPose = poseEstimator2.getFieldTags().getTagPose(tgt2.getFiducialId());
+            if (tagPose.isEmpty()){
+                continue;
+            }
+            numTags2++;
+            avgDist2 += tagPose.get().toPose2d().getTranslation().getDistance(estimatedPose.getTranslation());
+        }
+        for (var tgt3 : targets3){
+            var tagPose = poseEstimator3.getFieldTags().getTagPose(tgt3.getFiducialId());
+            if (tagPose.isEmpty()){
+                continue;
+            }
+            numTags3++;
+            avgDist3 += tagPose.get().toPose2d().getTranslation().getDistance(estimatedPose.getTranslation());
         }
 
-        if (numTags == 0)
+        if (numTags1 == 0){ //josh check (should check if 0 for other cams too?)
             return estStdDevs;
+        }
 
-        avgDist /= numTags;
+        avgDist1 /= numTags1;
+        avgDist2 /= numTags2;
+        avgDist3 /= numTags3;
 
-        // Multiple tags are more accurate
-        if (numTags > 1)
+        // Decrease std devs if multiple targets are visible
+        if (numTags1 > 1 || numTags2 > 1 || numTags3 > 1){ //josh check (check if other cams are greater than 1 too?)
             estStdDevs = Constants.VisionConstants.kMultiTagStdDevs;
-
-        // Reject single-tag far measurements
-        if (numTags == 1 && avgDist > 4)
-            return VecBuilder.fill(
-                Double.MAX_VALUE,
-                Double.MAX_VALUE,
-                Double.MAX_VALUE);
-
-        // Scale noise with distance
-        estStdDevs = estStdDevs.times(1 + (avgDist * avgDist / 30));
-
+        }
+        // Increase std devs based on (average) distance
+        if ((numTags1 == 1 && avgDist1 > 4) || (numTags2 == 1 && avgDist2 > 4) || (numTags3 == 1 && avgDist3 > 4)) {
+            estStdDevs = VecBuilder.fill(Double.MAX_VALUE, Double.MAX_VALUE, Double.MAX_VALUE);
+        } else if (avgDist1 < avgDist2 || avgDist1 < avgDist3) {
+            estStdDevs = estStdDevs.times(1 + (avgDist1 * avgDist1 / 30));
+        } else if (avgDist2 < avgDist1 || avgDist2 < avgDist3) {
+            estStdDevs = estStdDevs.times(1 + (avgDist2 * avgDist2 / 30));
+        } else if (avgDist3 < avgDist1 || avgDist3 < avgDist2) {
+            estStdDevs = estStdDevs.times(1 + (avgDist3 * avgDist3 / 30));
+        }
         return estStdDevs;
     }
 }
